ae037ee69ba0f735fa47c6108fa4f29e69e77fa3
LuisGomes18 <luiscgomes0@gmail.com>
2023-05-23 21:47:51 +0100

    Add files via upload

---
+++ Exercicio/exercicio_1/main.js
@@ -0,0 +1,8 @@
+var Num = prompt('Insira um número: ');
+Num = Number(Num);
+
+var Num_mais_Um = Num + 1;
+var Num_menos_Um = Num - 1;
+
+alert(`O sucessor de ${Num} é ${Num_mais_Um}`);
+alert(`E o antecessor de ${Num} é ${Num_menos_Um}`);
